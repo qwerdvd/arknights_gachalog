d64e4cf74e4886d832ec74d860f3158296f1286a
qwerdvd <2450899274@qq.com>
2022-10-28 20:21:00 +0800

    feat: 完成一部分抽卡记录的绘制

--- Arknights/src/plugins/ark/ark_gachalog/draw_gachalogs.py
+++ Arknights/src/plugins/ark/ark_gachalog/draw_gachalogs.py
@@ -1,16 +1,18 @@
 import json
 import asyncio
 import time
+from io import BytesIO
 from pathlib import Path
 from typing import Tuple, Union
 
+import matplotlib
 from nonebot.log import logger
 from PIL import Image, ImageDraw
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, font_manager
 
 from ..utils.draw_image_tools.send_image_tool import convert_img
 from ..utils.draw_image_tools.draw_image_tool import get_simple_bg
-from ..utils.fonts.fonts import font_origin
+from ..utils.fonts.fonts import font_origin, ys_font_origin
 from ..utils.alias.avatarId_and_name_covert import name_to_avatar_id
 # from ..utils.download_resource.RESOURCE_PATH import (
 #     CHAR_STAND_PATH as STAND_PATH,
@@ -26,7 +28,7 @@ TEXT_PATH = Path(__file__).parent / 'texture2d'
 title_mask = Image.open(TEXT_PATH / 'title_mask.png')
 up_tag = Image.open(TEXT_PATH / 'up.png')
 
-gs_font_16 = font_origin(16)
+gs_font_110 = ys_font_origin(110)
 gs_font_23 = font_origin(23)
 gs_font_26 = font_origin(26)
 gs_font_28 = font_origin(28)
@@ -85,7 +87,7 @@ async def _draw_card(
     item_pic = (
         Image.open(AVATAR_PATH / f'{_id}.jpg')
         .convert('RGBA')
-        .resize((108, 108))
+        .resize((300, 312))
     )
     card_img.paste(item_pic, point)
     # card_img.paste(item_pic, point, item_pic)
@@ -106,55 +108,56 @@ async def _draw_card(
 async def draw_gachalogs_img(uid: str) -> Union[bytes, str]:
     path = PLAYER_PATH / str(uid) / 'gacha_logs.json'
     if not path.exists():
-        return '你还没有抽卡数据噢~\n请添加token后使用命令`刷新抽卡记录`更新抽卡数据~'
+        return '你还没有抽卡数据噢~\n请添加cookie后使用命令`刷新抽卡记录`更新抽卡数据~'
     with open(path, 'r', encoding='UTF-8') as f:
         gacha_data = json.load(f)
 
     # 数据初始化
-    total_data = {'List': {
-        'total': 0,
-        'avg': 0,
-        'remain': 0,
-        # 'item': '',
-        'list': [],
-    }}
-    # for i in ['List']:
-    data_list = gacha_data['data']['List']
-    # logger.info(data_list)
-    is_not_first = False
-    num_temp = []
-    num = 1
-    for data in data_list[::-1]:
-        for j in range(len(data['chars'])):
-            if data['chars'][j]['rarity'] == 5:
-                data['gacha_num'] = num
-                # logger.info(data['gacha_num'])
-                # 排除第一个
-                if is_not_first:
-                    logger.info(num)
-                    num_temp.append(num)
-                if not num_temp:
-                    is_not_first = True
-                total_data['List']['list'].append(data)
-                num = 1
-                total_data['List']['total'] += 1
-            else:
-                num += 1
-    # total_data['List']['item'] = await name_to_avatar_id(total_data['List']['item'])
-    total_data['List']['remain'] = num - 1
-    if len(num_temp) == 0:
-        total_data['List']['avg'] = 0
-    else:
-        total_data['List']['avg'] = round(sum(num_temp) / len(num_temp), 2)  # type: ignore
-    logger.info(total_data)
+    total_data = {}
+    for i in ['单up池', '专属推荐干员寻访', '联合寻访', '常驻标准寻访']:
+        total_data[i] = {
+            'total': 0,
+            'avg': 0,
+            'remain': 0,
+            'item': '',
+            'list': [],
+        }
+        data_list = gacha_data['data'][i]
+        # logger.info(data_list)
+        is_not_first = False
+        num_temp = []
+        num = 1
+        for data in data_list[::-1]:
+            for j in range(len(data['chars'])):
+                if data['chars'][j]['rarity'] == 5:
+                    data['gacha_num'] = num
+                    # logger.info(data['gacha_num'])
+                    # 排除第一个
+                    if is_not_first:
+                        logger.info(num)
+                        num_temp.append(num)
+                    if not num_temp:
+                        is_not_first = True
+                    total_data[i]['list'].append(data)
+                    num = 1
+                    total_data[i]['total'] += 1
+                else:
+                    num += 1
+        # total_data['List']['item'] = await name_to_avatar_id(total_data['List']['item'])
+        total_data[i]['remain'] = num - 1
+        if len(num_temp) == 0:
+            total_data[i]['avg'] = 0
+        else:
+            total_data[i]['avg'] = round(sum(num_temp) / len(num_temp), 2)  # type: ignore
+        logger.info(total_data)
 
     # 常量偏移数据
-    single_y = 150
-    title_y = 300
+    single_y = 1000
+    title_y = 2000
 
     # 计算图片尺寸
     normal_y = (
-                       1 + ((total_data['List']['total'] - 1) // 6)
+                       1 + ((total_data['单up池']['total'] - 1) // 6)
                ) * single_y + title_y
     # char_y = (
     #     1 + ((total_data['角色祈愿']['total'] - 1) // 6)
@@ -164,7 +167,7 @@ async def draw_gachalogs_img(uid: str) -> Union[bytes, str]:
     # ) * single_y + title_y
 
     # 获取背景图片各项参数
-    based_w = 800
+    based_w = 2300
     based_h = normal_y + 200
     # based_h = normal_y + char_y + weapon_y + 200
     white_overlay = Image.new('RGBA', (based_w, based_h), (255, 255, 255, 220))
@@ -174,31 +177,49 @@ async def draw_gachalogs_img(uid: str) -> Union[bytes, str]:
     # 总标题
     all_title = Image.open(TEXT_PATH / 'all_title.png')
     all_title_draw = ImageDraw.Draw(all_title)
+    print(all_title_draw)
     # 写UID
-    all_title_draw.text((47, 181), f'{uid}', first_color, gs_font_26, 'lm')
+    all_title_draw.text((1450, 692), f'{uid}', first_color, gs_font_110, 'lm')
     bg_img.paste(all_title, (0, 0), all_title)
+    # 画饼图
+    Num = gacha_data['all_gacha_num']
+    six_star_num = gacha_data['six_star_gacha_num']
+    five_star_num = gacha_data['five_star_gacha_num']
+    four_star_num = gacha_data['four_star_gacha_num']
+    three_star_num = gacha_data['three_star_gacha_num']
+    data = [six_star_num, five_star_num, four_star_num, three_star_num]
+    explode = (0.1, 0.05, 0.025, 0)
+    labels = ['six_star', 'five_star', 'four_star', 'three_star']
+    colors = ['red', 'orange', 'yellow', 'green']
+    sizes = [data[0] / Num * 100, data[1] / Num * 100, data[2] / Num * 100, data[3] / Num * 100]
+    plt.pie(sizes, shadow=False, colors=colors, explode=explode, labels=labels)
+    plt.legend()
+    plt.axis('square')
+    plt.savefig('pie.png', format='png', bbox_inches='tight', transparent=True, dpi=600, facecolor='none')
+    pie = Image.open('./pie.png').resize((800, 800))
+    bg_img.paste(pie, (100, 875), pie)
 
     # 处理title
     # {'total': 0, 'avg': 0, 'remain': 0, 'list': []}
-    type_list = ['List']
+    type_list = ['单up池', '专属推荐干员寻访', '联合寻访', '常驻标准寻访']
     y_extend = 0
     for index, i in enumerate(type_list):
-        title = Image.open(TEXT_PATH / 'gahca_title.png')
-        # title_pic = (
-        #     Image.open(STAND_PATH / f'{total_data[i]["item"]}.png')
-        #     .convert('RGBA')
-        #     .resize((1292, 792))
+        # title = Image.open(TEXT_PATH / 'gahca_title.png')
+        # # title_pic = (
+        # #     Image.open(STAND_PATH / f'{total_data[i]["item"]}.png')
+        # #     .convert('RGBA')
+        # #     .resize((1292, 792))
+        # # )
+        # temp_pic = Image.new('RGBA', (800, 300), (0, 0, 0, 0))
+        # # temp_pic.paste(title_pic, (-70, -56), title_pic)
+        # temp_mask = Image.new('RGBA', (800, 300), (0, 0, 0, 0))
+        # temp_mask.paste(temp_pic, (0, 0), title_mask)
+        # temp_mask.putalpha(
+        #     temp_mask.getchannel('A').point(
+        #         lambda x: round(x * 0.6) if x > 0 else 0
+        #     )
         # )
-        temp_pic = Image.new('RGBA', (800, 300), (0, 0, 0, 0))
-        # temp_pic.paste(title_pic, (-70, -56), title_pic)
-        temp_mask = Image.new('RGBA', (800, 300), (0, 0, 0, 0))
-        temp_mask.paste(temp_pic, (0, 0), title_mask)
-        temp_mask.putalpha(
-            temp_mask.getchannel('A').point(
-                lambda x: round(x * 0.6) if x > 0 else 0
-            )
-        )
-        title = Image.alpha_composite(title, temp_mask)
+        # title = Image.alpha_composite(title, temp_mask)
 
         if total_data[i]['avg'] == 0:
             level = 3
@@ -218,29 +239,29 @@ async def draw_gachalogs_img(uid: str) -> Union[bytes, str]:
             else:
                 level = 3
 
-        tag_pic = await _get_tag(level)
-        tag_pic = tag_pic.resize((208, 88))
-        title.paste(tag_pic, (35, 54), tag_pic)
-        title_draw = ImageDraw.Draw(title)
-        # 卡池
-        title_draw.text((245, 86), i, first_color, gs_font_40, 'lm')
+        # tag_pic = await _get_tag(level)
+        # tag_pic = tag_pic.resize((208, 88))
+        # # title.paste(tag_pic, (35, 54), tag_pic)
+        # title_draw = ImageDraw.Draw(title)
+        # # 卡池
+        # title_draw.text((245, 86), i, first_color, gs_font_40, 'lm')
         # 抽卡时间
-        if gacha_data['data'][i]:
-            ts = gacha_data['data'][i][0]['ts']
-            # first_time = datetime.datetime.strptime(first_timestamp, '%Y-%m-%d')
-            local_gacha_time = time.localtime(ts)
-            first_time = time.strftime('%Y-%m-%d %H:%M:%S', local_gacha_time)
-        else:
-            first_time = '暂未抽过卡!'
-        title_draw.text((245, 123), first_time, first_color, gs_font_28, 'lm')
+        # if gacha_data['data'][i]:
+        #     ts = gacha_data['data'][i][0]['ts']
+        #     # first_time = datetime.datetime.strptime(first_timestamp, '%Y-%m-%d')
+        #     local_gacha_time = time.localtime(ts)
+        #     first_time = time.strftime('%Y-%m-%d %H:%M:%S', local_gacha_time)
+        # else:
+        #     first_time = '暂未抽过卡!'
+        # title_draw.text((245, 123), first_time, first_color, gs_font_28, 'lm')
         # 平均抽卡数量
-        title_draw.text(
-            (108, 209),
-            str(total_data[i]['avg']),
-            first_color,
-            gs_font_40,
-            'mm',
-        )
+        # title_draw.text(
+        #     (108, 209),
+        #     str(total_data[i]['avg']),
+        #     first_color,
+        #     gs_font_40,
+        #     'mm',
+        # )
         # title_draw.text(
         #     (261, 209),
         #     str(gacha_data[f'{CHANGE_MAP[i]}_gacha_num']),
@@ -248,24 +269,26 @@ async def draw_gachalogs_img(uid: str) -> Union[bytes, str]:
         #     gs_font_40,
         #     'mm',
         # )
-        title_draw.text(
-            (104, 160),
-            str(total_data[i]['remain']),
-            red_color,
-            gs_font_28,
-            'mm',
-        )
+        # title_draw.text(
+        #     (104, 160),
+        #     str(total_data[i]['remain']),
+        #     red_color,
+        #     gs_font_28,
+        #     'mm',
+        # )
         y_extend += (
-            (1 + ((total_data[type_list[index - 1]]['total'] - 1) // 6)) * 150
+            (1 + ((total_data[type_list[index - 1]]['total'] - 1) // 6)) * 50
             if index != 0
             else 0
         )
-        y = index * title_y + y_extend + 200
-        bg_img.paste(title, (0, y), title)
+        y = y_extend + 10
+        # y = index * title_y + y_extend + 500
+        # bg_img.paste(title, (0, y), title)
+        # bg_img.paste(title, (0, y), title)
         tasks = []
         for item_index, item in enumerate(total_data[i]['list']):
-            item_x = (item_index % 6) * 129 + 20
-            item_y = (item_index // 6) * 150 + y + title_y
+            item_x = (item_index % 6) * 350 + 100
+            item_y = (item_index // 6) * 419 + y + title_y
             xy_point = (item_x, item_y)
             tasks.append(
                 _draw_card(
